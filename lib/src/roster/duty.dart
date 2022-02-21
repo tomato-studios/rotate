c3d87c5a688ec0f48dff75ebb92d25b53cad7717
Max Beckenbauer <max.bec92@gmail.com>
2022-02-21 16:59:51 +0100

    Implementation of Duty class.
    Signed-off-by: Max Beckenbauer <max.bec92@gmail.com>

--- lib/src/roster/duty.dart
+++ lib/src/roster/duty.dart
@@ -2,19 +2,7 @@
 // for details. All rights reserved.
 
 import 'package:flutter/material.dart';
-import 'package:rotate/src/roster/duty.dart';
 import 'package:rotate/src/user/user.dart';
 
 /// Something that's being tracked, e.g. Hours Slept, Cups of water, etc.
-class Roster {
-  User user = User();
-
-  String? baseLocator;
-  String? name;
-  String? pkNumber;
-
-  DateTime? generationTime;
-  DateTimeRange? dateRange;
-
-  Iterable<Duty> dutyList = Iterable.empty();
-}
+class Duty {}
