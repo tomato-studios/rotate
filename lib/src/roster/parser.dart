1734e85d52f3a53b04c6d10a5e05c5f8b806dee1
Max Beckenbauer <max.bec92@gmail.com>
2022-02-24 08:50:02 +0100

    Added airline config file for CFG
    Signed-off-by: Max Beckenbauer <max.bec92@gmail.com>

--- lib/src/roster/parser.dart
+++ lib/src/roster/parser.dart
@@ -1,41 +1,64 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:domain_model/domain_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
-import 'package:rotate/src/roster/duty.dart';
+import 'package:rotate/shared/logger.dart';
 import 'package:rotate/src/roster/roster.dart';
 import 'package:syncfusion_flutter_pdf/pdf.dart';
-
-import 'package:logger/logger.dart';
-
-var logger = Logger(
-  printer: PrettyPrinter(),
-);
 
 ///
 /// Helper class for Parsing operations
 ///
 class ParserUtils {
   // Regex Strings
-  static final regexNewLine = RegExp(r'\r\n|\n|\r');
-  static final regexPkLine = RegExp(r'(\w{3})CFG\/CREW\/(\d*\w{1})');
-  static final regexIdLine = RegExp(
+  final regexNewLine = RegExp(r'\r\n|\n|\r');
+  final regexPkLine = RegExp(r'(\w{3})CFG\/CREW\/(\d*\w{1})');
+  final regexIdLine = RegExp(
       r'^Individualdutyplanfor\d*\w{1}(.*),(.*)NetLine.*CREWLINK((\d{2})(\D{3})(\d{2})).*Period:((\d{2})(\D{3})(\d{2}))-((\d{2})(\D{3})(\d{2}))');
-  static final regexDutyLines = RegExp(
+  final regexDutyLines = RegExp(
       r'(?<Date>(?:Mon|Tue|Wed|Thu|Fri|Sat|Sun)(?<Day>\d{2}))(?:(?<DutyCode>\S+)(?<Airport>[A-Z]{3})(?:(?<DepTime>\d{4})(?<ArrTime>\d{4}))?(?<AcType>\w*)?|X).*?(?:\[(?<FT>FT\d{1,2}:\d{2})\])?(?:\s*?\[(?<DP>DP\d{1,2}:\d{2})\])?(?:\s*?\[(?<FDP>FDP\d{1,2}:\d{2})\])?(?:\s*?\[(?<RT>RT\d{1,2}:\d{2})\])?(?:\s*?\[(?<mFDP>mFDP\d{1,2}:\d{2})\])?(?<Comment>.*?)(?:\[(?<EQ>EQ\d{1,2}:\d{2})\]).*?(?:\[(?<BZW>BZW\d{1,2}:\d{2})\])');
-  static final regexNameLines = RegExp(
+  final regexNameLines = RegExp(
       r'(?<Date>(?:Mon|Tue|Wed|Thu|Fri|Sat|Sun)\d{2})(?:(?<DutyCode>\S+)(?<Airport>[A-Z]{3})(?:(?<DepTime>\d{4})(?<ArrTime>\d{4}))?)\s*?crewonevent:(?<CrewNames>.*?)(?=Mon|Tue|Wed|Thu|Fri|Sat|Sun|RecurrentTraining)');
 
-  static final DateFormat dateFormat = DateFormat("ddMMMyy");
+  final DateFormat dateFormat = DateFormat("ddMMMyy");
+  final DateFormat formatDepTime = DateFormat('hhmm');
+
+  // Regex Groups
+  final regexGroupDate = 'Date';
+  final regexGroupDay = 'Day';
+  final regexGroupDutyCode = 'DutyCode';
+  final regexGroupAirport = 'Airport';
+  final regexGroupDepTime = 'DepTime';
+  final regexGroupArrTime = 'ArrTime';
+  final regexGroupAcType = 'AcType';
+  final regexGroupFT = 'FT';
+  final regexGroupDP = 'DP';
+  final regexGroupFDP = 'FDP';
+  final regexGroupRT = 'RT';
+  final regexGroupMFDP = 'mFDP';
+  final regexGroupComment = 'Comment';
+  final regexGroupEQ = 'EQ';
+  final regexGroupBZW = 'BZW';
+
+  // Duty codes
+  static const List<String> _offCodes = <String>['OFF', 'ORT', '-'];
+  static const List<String> _hotCodes = <String>['HOT'];
+  static const List<String> _sbyCodes = <String>['WARPX_3', 'RC'];
+  static const List<String> _resCodes = <String>['WARPX_3', 'RC'];
+  static const List<String> _fltCodes = <String>['WARPX_3', 'RC'];
+  static const List<String> _gndCodes = <String>['GC_NL', 'RFT', 'DGR'];
+  static const List<String> _trvCodes = <String>['WARPX_3', 'RC'];
+  static const List<String> _simCodes = <String>['WARPX_3', 'RC'];
+
 
   // For Debug only
   static const testRoster = 'Roster_22_03.pdf';
 
   // TODO: Check if Roster already exists, if yes update
-
-  static Future<void> extractAllText() async {
+  Future<void> extractAllText() async {
     //Load the existing PDF document.
     PdfDocument document =
         PdfDocument(inputBytes: await _readDocumentData(testRoster));
@@ -49,7 +72,7 @@ class ParserUtils {
     _parseRosterData(text);
   }
 
-  static void _parseRosterData(String rawText) {
+  void _parseRosterData(String rawText) {
     Roster newRoster = Roster();
 
     // Debug Log
@@ -65,12 +88,12 @@ class ParserUtils {
     _parseNames(rawText, newRoster);
   }
 
-  static Future<List<int>> _readDocumentData(String name) async {
+  Future<List<int>> _readDocumentData(String name) async {
     final ByteData data = await rootBundle.load('assets/$name');
     return data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
   }
 
-  static void _parsePkLine(String rawText, Roster newRoster) async {
+  void _parsePkLine(String rawText, Roster newRoster) {
     // First Line (PK-Line)
     Iterable<String> list = LineSplitter.split(rawText);
     RegExpMatch? matches = regexPkLine.firstMatch(list.elementAt(0));
@@ -78,7 +101,7 @@ class ParserUtils {
     newRoster.pkNumber = matches?.group(2);
   }
 
-  static void _parseIdLine(String rawText, Roster newRoster) async {
+  void _parseIdLine(String rawText, Roster newRoster) {
     // First Line (PK-Line)
     Iterable<String> list = LineSplitter.split(rawText);
     RegExpMatch? matches = regexIdLine.firstMatch(list.elementAt(1));
@@ -90,24 +113,44 @@ class ParserUtils {
         end: dateFormat.parse(matches.group(11)!));
   }
 
-  static Future<Iterable<RegExpMatch>> _parseDuties(
+  Future<Iterable<RegExpMatch>> _parseDuties(
       String rawText, Roster newRoster) async {
     // Duty-List
-    rawText = rawText.replaceAll(regexNewLine, " ");
-    Iterable<RegExpMatch> dutyList = regexDutyLines.allMatches(rawText);
+    final rawTextWithoutNewlines = rawText.replaceAll(regexNewLine, ' ');
+    final Iterable<RegExpMatch> dutyList =
+        regexDutyLines.allMatches(rawTextWithoutNewlines);
 
-    dutyList.forEach((element) {
-      Duty currDuty;
+    for (final element in dutyList) {
+      final dutyCode = element.namedGroup(regexDutyCode);
+
+      if (_offCodes.contains(dutyCode)) {
+        DateTime(
+          newRoster.dateRange!.start.year,
+          newRoster.dateRange!.start.month,
+          int.parse(element.namedGroup('Day')!)));
+        Duty duty = Duty.off(dutyCode: dutyCode, start: element.namedGroup(regexGroupDepTime), end: end)
+
+      } else if (_gndCodes.contains(dutyCode)) {
+
+      } else if (_simCodes.contains(dutyCode)) {
+
+      } else {
+
+      }
+      currDuty.date(DateTime(
+          newRoster.dateRange!.start.year,
+          newRoster.dateRange!.start.month,
+          int.parse(element.namedGroup('Day')!)));
       logger.e(element.namedGroup("Date"));
       for (var i = 0; i < element.groupCount; i++) {
         logger.d(element.group(i));
       }
-    });
+    }
 
     return dutyList;
   }
 
-  static Future<Iterable<RegExpMatch>> _parseNames(
+  Future<Iterable<RegExpMatch>> _parseNames(
       String rawText, Roster newRoster) async {
     // Duty-List
     rawText = rawText.replaceAll(regexNewLine, " ");
@@ -122,5 +165,13 @@ class ParserUtils {
     });
 
     return nameList;
+  }
+
+  DateTime _getDateTimeFromDay(int day) {
+    DateTime returnTime = DateTime(
+          newRost,
+          newRoster.dateRange!.start.month,
+          day);
+    return returnTime;
   }
 }
